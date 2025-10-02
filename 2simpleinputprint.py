c634f238851b357e5336ad4b7b4f88f2be734106
Durva Chauhan <chauhandurva8@gmail.com>
2025-10-03 00:09:40 +0530

    Add files via upload

---
+++ 2simpleinputprint.py
@@ -0,0 +1,17 @@
+name=input("Enter your name :")
+mobile=input("Enter your mobile number :")
+email=input("Enter your E-mail :")
+
+print("Name:",name)
+print("Moble number:",mobile)
+print("E-mail:",email)
+
+
+
+#output
+#Enter your name :Durva
+#Enter your mobile number :1234567890
+#Enter your E-mail :abc@gmail.com
+#Name: Durva
+#Moble number: 1234567890
+#E-mail: abc@gmail.com
