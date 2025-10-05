bb29a4dcec108c1bed49a7fa600808a6647346c8
Durva Chauhan <chauhandurva8@gmail.com>
2025-10-05 20:39:38 +0530

    Add files via upload

---
+++ 3sumof2.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the value of a :"))
+b=int(input("Enter the value of b :"))
+
+c=a + b
+
+print("Sum="+str(c))
+
+
+#output
+#Enter the value of a :2
+#Enter the value of b :3
+#Sum=5
