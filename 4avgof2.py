8b978b6828a772458fa278db2b7d7a6d36f56c08
Durva Chauhan <chauhandurva8@gmail.com>
2025-10-06 16:25:53 +0530

    Add files via upload

---
+++ 4avgof2.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the value of a :"))
+b=int(input("Enter the value of b :"))
+
+avg=(a+b) / 2
+
+print("average:"+str(avg))
+
+
+#output
+#Enter the value of a :4
+#Enter the value of b :5
+#average:4.5
