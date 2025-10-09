2e6950e0b5370829bc5919b54938c228ffe64cc9
Durva Chauhan <chauhandurva8@gmail.com>
2025-10-09 21:44:26 +0530

    Add files via upload

---
+++ 5slope.py
@@ -0,0 +1,16 @@
+x1=int(input("Enter x1 :"))
+y1=int(input("Enter y1 :"))
+x2=int(input("Enter x2 :"))
+y2=int(input("Enter y2 :"))
+
+slope=(y2-y1) / (x2-x1)
+
+print("the slope is:"+str(slope))
+
+
+#output
+#Enter x1 :1
+#Enter y1 :2
+#Enter x2 :3
+#Enter y2 :4
+#the slope is:1.0
